ec143f5a00eb3888c70a387c3d612c2fd5670c1c
HiteshAttri <hiteshattri71@gmail.com>
2023-02-24 18:26:05 +0530

    frontend part done

---
+++ script.js
@@ -0,0 +1,14 @@
+let sessionTimeP =  document.getElementById('session-time-val');
+let breakTimeP =  document.getElementById('break-time-val');
+
+let sessionTimeMinusBtn = document.getElementById('session-time-Mbtn');
+let sessionTimePlusBtn = document.getElementById('session-time-Pbtn');
+
+let breakTimeMinusBtn = document.getElementById('break-time-Mbtn');
+let breakTimePlusBtn = document.getElementById('break-time-Pbtn');
+
+let showHerePtag = document.getElementById('show-here');
+
+let isRunning = false;
+
+
